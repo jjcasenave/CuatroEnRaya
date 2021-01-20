97af2efb7e594de2643507301e9567cbb6a9bada
Masenko <juanjosecasenavealonso@gmail.com>
2021-01-20 11:41:29 +0100

    Creados getters y setters, constructor por defecto, metodo estaOcupada

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -2,5 +2,44 @@ package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public class Casilla {
 	
+	//atributos
+	private Ficha ficha;
 
+	//metodos
+	public boolean estaOcupada() {
+		if (ficha==null)
+			return false;
+		else
+			return true;		
+	}
+	
+	//setters y getters
+	public Ficha getFicha() {
+		return ficha;
+	}
+
+	public void setFicha(Ficha ficha) {
+		if (ficha==null)
+			throw new NullPointerException("ERROR");
+		else
+			this.ficha = ficha;
+	}
+	
+	//constructor por defecto
+	public Casilla() throws IllegalArgumentException
+	{
+		ficha=null;
+	}
+
+	@Override
+	public String toString() {
+		if (ficha==null) 
+			return " ";
+	else 
+			return String.format("%.1s", ficha);
+			
+	}
+	
+	
+	
 }
