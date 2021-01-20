98998b5ebd2ee3b56d16ffef9eb3e5cb04e04725
Masenko <juanjosecasenavealonso@gmail.com>
2021-01-20 12:43:40 +0100

    terminada clase Casilla con test pasados y completados con exito

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -1,45 +1,56 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Casilla {
-	
-	//atributos
+
+	// atributos
 	private Ficha ficha;
 
-	//metodos
+	// metodos
 	public boolean estaOcupada() {
-		if (ficha==null)
-			return false;
-		else
-			return true;		
+
+		boolean casillaOcupada = false;
+
+		if (this.ficha == Ficha.AZUL || this.ficha == Ficha.VERDE) {
+			casillaOcupada = true;
+		}
+		return casillaOcupada;
 	}
-	
-	//setters y getters
+
+	// setters y getters
 	public Ficha getFicha() {
 		return ficha;
 	}
 
-	public void setFicha(Ficha ficha) {
-		if (ficha==null)
-			throw new NullPointerException("ERROR");
-		else
-			this.ficha = ficha;
+	public void setFicha(Ficha ficha) throws OperationNotSupportedException {
+		if (estaOcupada() == false) {
+			if (ficha == null) {
+				throw new NullPointerException("ERROR: No se puede poner una ficha nula.");
+			} else if (ficha == Ficha.AZUL) {
+				this.ficha = ficha.AZUL;
+			} else if (ficha == Ficha.VERDE) {
+				this.ficha = ficha.VERDE;
+			} else {
+				throw new IllegalArgumentException("ERROR:el color escogido es incorrecto.");
+			}
+		} else {
+			throw new OperationNotSupportedException("ERROR: Ya contengo una ficha.");
+		}
 	}
-	
-	//constructor por defecto
-	public Casilla() throws IllegalArgumentException
-	{
-		ficha=null;
+
+	// constructor por defecto
+	public Casilla() throws IllegalArgumentException {
+		ficha = null;
 	}
 
 	@Override
 	public String toString() {
-		if (ficha==null) 
+		if (ficha == null)
 			return " ";
-	else 
+		else
 			return String.format("%.1s", ficha);
-			
+
 	}
-	
-	
-	
+
 }
