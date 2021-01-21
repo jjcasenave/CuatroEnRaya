fd28d1357ec0df30deee8df257781fd498b4827b
MasenkoPC <MasenkoPC@192.168.1.146>
2021-01-22 00:19:25 +0100

    Clase jugador completada, test exitosos todos los metodos

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Jugador.java
@@ -1,42 +1,54 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
 public class Jugador {
-	
-	//atributos
-	private Ficha ficha;
+
+	// atributos
+	private Ficha colorFichas;
 	private String nombre;
-	
-	//constructor con parametros
-	
-	public Jugador(String nombre, Ficha ficha)	{
+
+	// constructor con parametros
+
+	public Jugador(String nombre, Ficha colorFichas) {
 		setNombre(nombre);
-		setFicha(ficha);
+		setColorFichas(colorFichas);
 	}
-	
-	//metodos
-	
-	//setters y getters
-	public Ficha getFicha() {
-		return ficha;
+
+	// metodos
+
+	// setters y getters
+	public Ficha getColorFichas() {
+		return colorFichas;
 	}
-	private void setFicha(Ficha ficha) {
-		this.ficha = ficha;
+
+	private void setColorFichas(Ficha colorFichas) {
+		if(colorFichas==null) {
+			throw new NullPointerException("ERROR: El color de las fichas no puede ser nulo.");
+		}else {
+			this.colorFichas = colorFichas;
+		}		
+		
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	private void setNombre(String nombre) {
-		this.nombre = nombre;
+		if (nombre == null) {
+			throw new NullPointerException("ERROR: El nombre no puede ser nulo.");
+		} else if (nombre.matches(" ")) {
+			throw new IllegalArgumentException("ERROR: El nombre no puede estar vacío.");
+		} else if (nombre.matches("") || nombre.matches("\s+")) {
+			throw new IllegalArgumentException("ERROR: El nombre no puede estar vacío.");
+		} else {
+			this.nombre = nombre;
+		}
+
 	}
 
 	@Override
 	public String toString() {
-		return String.format("%s (%s)", nombre, ficha);
+		return String.format("%s (%s)", nombre, colorFichas);
 	}
-
-	
-	
-	
-	
 
 }
