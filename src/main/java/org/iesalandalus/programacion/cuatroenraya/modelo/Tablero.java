9c643b85414f3940cf29877257f97442ba0c5379
Masenko <juanjosecasenavealonso@gmail.com>
2021-01-30 18:26:43 +0100

    creados metodos comprobarFicha y comprobarColumna

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Tablero.java
@@ -62,44 +62,60 @@ public class Tablero {
 		return lleno;
 	}
 
+	private void comprobarFicha(Ficha ficha) {
+
+		if (!(ficha == ficha.AZUL || ficha == ficha.VERDE)) {
+			throw new IllegalArgumentException("ERROR: no es valido ");
+		}
+
+	}
+
+	private void comprobarColumna(int columna) {
+		// si la columna no es correcta, lanza excepcion
+
+		if (!(columna < 0 || columna > 6))
+			throw new IllegalArgumentException("ERROR: ");
+
+	}
+
 }
 
 /*
- *
  * 
- * Crea el método estaLleno que devolverá true o false dependiendo de que el
- * tablero esté lleno o no. Este método hará uso del método anteriormente
- * creado.
+ * Crea el método getPrimeraFilaVacia que devolverá la primera fila vacía para
+ * la columna pasada por parámetro.
  * 
- * Crea el método comprobaFicha que lanzará una excepción si el parámetro pasado
- * no es correcto.
+ * Crea el método objetivoAlcanzado que aceptará como parámetro el número de
+ * fichas consecutivas que llevamos en una de las comprobaciones (horizontal,
+ * vertical, diagonalNE o diagonalNO) y devolverá true o false si el parámetro
+ * pasado es mayor o igual que la constante definida en la clase para la fichas
+ * conscutivas necesarias.
  * 
- * Crea el método comprobarColumna que lanzará una excepción si el parámetro
- * pasado no es correcto. Crea el método getPrimeraFilaVacia que devolverá la
- * primera fila vacía para la columna pasada por parámetro. Crea el método
- * objetivoAlcanzado que aceptará como parámetro el número de fichas
- * consecutivas que llevamos en una de las comprobaciones (horizontal, vertical,
- * diagonalNE o diagonalNO) y devolverá true o false si el parámetro pasado es
- * mayor o igual que la constante definida en la clase para la fichas
- * conscutivas necesarias. Crea el método comprobarHorizontal que aceptará la
- * fila y la ficha para la que queremos comprobar si hay cuatro fichas del mismo
- * color seguidas en horizontal. Crea el método comprobarVertical que aceptará
- * la columna y la ficha para la que queremos comprobar si hay cuatro fichas del
- * mismo color seguidas en vertical. Crea el método menor que recibirá dos
- * enteros y devolverá el menor de ellos. Crea el método comprobarDiagonalNE que
- * recibirá como parámetros la fila, la columna y la ficha para la que queremos
- * comprobar si hay cuatro fichas del mismo color seguidas en la diagonal que va
- * desde abajo a la izquierda hasta arriba a la derecha pasando por la casilla
- * indicada por la fila y la columna. Para ayudarte, te indico que debes
- * calcular el desplazamiento inicial como el mínimo entre la fila y la columna
- * pasadas. Luego calcularás la fila inicial como la fila pasada menos el
- * desplazamiento inicial y la columna inicial como la columna pasada menos el
- * desplazamiento inicial. Luego realizarás un bucle que empezando en la fila
- * inicial y la columna inicial y terminando cuando lleguemos al límite de las
- * filas o de las columnas, incremente en uno la fila y la columna (las
- * variables de control del bucle) y cuente cuántas fichas de ese color hay
- * seguidas. Devolverá true si las fichas seguidas contadas es igual a 4. Crea
- * el método comprobarDiagonalNO que recibirá como parámetros la fila, la
+ * Crea el método comprobarHorizontal que aceptará la fila y la ficha para la
+ * que queremos comprobar si hay cuatro fichas del mismo color seguidas en
+ * horizontal.
+ * 
+ * Crea el método comprobarVertical que aceptará la columna y la ficha para la
+ * que queremos comprobar si hay cuatro fichas del mismo color seguidas en
+ * vertical.
+ * 
+ * Crea el método menor que recibirá dos enteros y devolverá el menor de ellos.
+ * 
+ * Crea el método comprobarDiagonalNE que recibirá como parámetros la fila, la
+ * columna y la ficha para la que queremos comprobar si hay cuatro fichas del
+ * mismo color seguidas en la diagonal que va desde abajo a la izquierda hasta
+ * arriba a la derecha pasando por la casilla indicada por la fila y la columna.
+ * Para ayudarte, te indico que debes calcular el desplazamiento inicial como el
+ * mínimo entre la fila y la columna pasadas. Luego calcularás la fila inicial
+ * como la fila pasada menos el desplazamiento inicial y la columna inicial como
+ * la columna pasada menos el desplazamiento inicial. Luego realizarás un bucle
+ * que empezando en la fila inicial y la columna inicial y terminando cuando
+ * lleguemos al límite de las filas o de las columnas, incremente en uno la fila
+ * y la columna (las variables de control del bucle) y cuente cuántas fichas de
+ * ese color hay seguidas. Devolverá true si las fichas seguidas contadas es
+ * igual a 4.
+ * 
+ * Crea el método comprobarDiagonalNO que recibirá como parámetros la fila, la
  * columna y la ficha para la que queremos comprobar si hay cuatro fichas del
  * mismo color seguidas en la diagonal que va desde abajo a la derecha hasta
  * arriba a la izquierda pasando por la casilla indicada por la fila y la
@@ -112,17 +128,21 @@ public class Tablero {
  * las filas o 0 para las columnas, incremente en uno la fila y decremente en
  * uno la columna (las variables de control del bucle) y cuente cuántas fichas
  * de ese color hay seguidas. Devolverá true si las fichas seguidas contadas es
- * igual a 4. Crea el método comprobarTirada que aceptará como parámetros la
- * fila, la columna y la ficha de la última tirada para comprobar si es ganadora
- * o no. Este método hará uso de los métodos comprobarXXX anteriormente
- * definidos. Crea el método introducirFicha que aceptará como parámetros la
- * columna en la que queremos introducir la ficha y la ficha a introducir y
- * devolverá true o false dependiendo de si la jugada ha sido ganadora o no
- * (hemos conseguido cuatro en raya o no). Este método deberá comprobar la
- * validez de los parámetros pasados o lanzar la excepción correspondiente.
- * También podrá lanzar una excepción indicando que la fila en la que queremos
- * introducir la ficha está llena. Para ello este método hará uso de los métodos
- * anteriormente creados. Crea el método toString que haciendo uso de la clase
- * StringBuilder devuelva la representación del tablero que has visto en las
- * imágenes anteriores.
+ * igual a 4.
+ * 
+ * Crea el método comprobarTirada que aceptará como parámetros la fila, la
+ * columna y la ficha de la última tirada para comprobar si es ganadora o no.
+ * Este método hará uso de los métodos comprobarXXX anteriormente definidos.
+ * 
+ * Crea el método introducirFicha que aceptará como parámetros la columna en la
+ * que queremos introducir la ficha y la ficha a introducir y devolverá true o
+ * false dependiendo de si la jugada ha sido ganadora o no (hemos conseguido
+ * cuatro en raya o no). Este método deberá comprobar la validez de los
+ * parámetros pasados o lanzar la excepción correspondiente. También podrá
+ * lanzar una excepción indicando que la fila en la que queremos introducir la
+ * ficha está llena. Para ello este método hará uso de los métodos anteriormente
+ * creados.
+ * 
+ * Crea el método toString que haciendo uso de la clase StringBuilder devuelva
+ * la representación del tablero que has visto en las imágenes anteriores.
  */
