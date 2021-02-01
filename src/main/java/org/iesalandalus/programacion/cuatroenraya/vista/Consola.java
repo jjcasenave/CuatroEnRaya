553283610bab5c177774fc5c9f69f2305a8d6bd6
MasenkoPC <MasenkoPC@192.168.1.146>
2021-02-01 13:08:42 +0100

    Creada clase consola, falta desarrollar los metodos

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/vista/Consola.java
@@ -0,0 +1,40 @@
+package org.iesalandalus.programacion.cuatroenraya.vista;
+
+import org.iesalandalus.programacion.cuatroenraya.modelo.Ficha;
+import org.iesalandalus.programacion.cuatroenraya.modelo.Jugador;
+
+public class Consola {
+	private Consola() {
+
+	}
+
+	public static String leerNombre() {
+		return null;
+	}
+
+	public static Ficha elegirColorFichas() {
+		return null;
+	}
+	
+	public static Jugador leerJugador() {
+		return null;		
+	}
+	
+	public static Jugador leerJugador(Ficha ficha) {
+		return null;
+	}
+	
+	public static int leerColumna(Jugador jugador) {
+		return 0;
+		
+	}
+
+}
+
+/*
+ * Crea el método leerColumna que recibirá como parámetro el jugador que debe
+ * introducir la columna para una tirada y devolverá un entero representando la
+ * columna elegida. El método imprimirá el nombre del jugador y le indicará que
+ * elija la columna en la que quiere introducir su ficha y esto lo repetirá
+ * mientras la columna elegida no sea válida.
+ */
