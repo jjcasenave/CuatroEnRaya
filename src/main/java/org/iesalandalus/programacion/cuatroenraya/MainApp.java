95d8f02ea5cb29e0f9fbdefbdb4d10e5157bc1b4
MasenkoPC <MasenkoPC@192.168.1.146>
2021-02-02 00:30:00 +0100

    pequeño fix en metodo main

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/MainApp.java
@@ -6,8 +6,9 @@ import org.iesalandalus.programacion.cuatroenraya.vista.Consola;
 public class MainApp {
 
 	public static void main(String[] args) {
+		
 		Jugador jugador1 = Consola.leerJugador();
-		Jugador jugador2 = Consola.leerColumna(jugador1.getColorFichas());
+		Jugador jugador2 = Consola.leerJugador(jugador1.getColorFichas());
 		CuatroEnRaya cuatroEnRaya = new CuatroEnRaya(jugador1, jugador2);
 		cuatroEnRaya.jugar();
 
